45a81888cf72774b858d8cb0007749b02d52c8c3
Brian O'Connor <briandoconnor@gmail.com>
2015-05-26 17:35:26 -0400

    working on script

--- 20150526_fixing_pancancer_metadata_problem_sergei/scripts/generate_fix.pl
+++ 20150526_fixing_pancancer_metadata_problem_sergei/scripts/generate_fix.pl
@@ -7,9 +7,10 @@ if (!-e "donor_p_150526020206.jsonl.gz") { system("wget http://pancancer.info/gn
 open IN, "gunzip -c donor_p_150526020206.jsonl.gz | " or die "Can't open donor_p_150526020206.jsonl.gz";
 
 while (<IN>) {
-  my $jd = decode_json ($_);
-  print Dumper $jd;
-  die;
+  if (/ce799e7b-30e7-44a5-a185-3e50d5e059ef/) {
+    my $jd = decode_json ($_);
+    print Dumper $jd;
+  }
 }
 
 close IN;
